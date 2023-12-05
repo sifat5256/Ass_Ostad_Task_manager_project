8aba6526485699c9924948f684dd92b716e56a60
sifat5256 <iqramulhasan65@gmail.com>
2023-12-05 22:58:38 +0600

    updated

--- lib/screen/main_button_nav_screen.dart
+++ lib/screen/main_button_nav_screen.dart
@@ -39,7 +39,7 @@ class _MainbuttonnavscreenState extends State<Mainbuttonnavscreen> {
         unselectedItemColor: Colors.grey,
         showUnselectedLabels: true,
         items: [
-          BottomNavigationBarItem(icon:Icon(Icons.abc),label: "New"),
+          BottomNavigationBarItem(icon:Icon(Icons.ad_units),label: "New"),
           BottomNavigationBarItem(icon: Icon(Icons.change_circle_outlined),label: "In progrss"),
           BottomNavigationBarItem(icon:Icon(Icons.done),label: "Completed"),
           BottomNavigationBarItem(icon: Icon(Icons.close),label: "Cancelled"),
